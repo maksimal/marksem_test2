9049d27adbea21302c199aa3557f30b26f342173
Maksym PC home <malishev.maksim@gmail.com>
2020-04-09 22:42:26 +0300

    Updates

---
+++ precache-manifest.4b6f3f24cc4b2cc62fb4b67ba19b59a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a3d1efe365c5d36b010c025f3e5ac1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c314c99a493d0c718e6",
+    "url": "/static/css/2.a33d7f97.chunk.css"
+  },
+  {
+    "revision": "311b4a3066db0e0a54db",
+    "url": "/static/css/main.971c94a2.chunk.css"
+  },
+  {
+    "revision": "8c314c99a493d0c718e6",
+    "url": "/static/js/2.152034ab.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.152034ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "311b4a3066db0e0a54db",
+    "url": "/static/js/main.179f3378.chunk.js"
+  },
+  {
+    "revision": "482659de96e4fa5d6cd7",
+    "url": "/static/js/runtime-main.77c57556.js"
+  }
+]);
